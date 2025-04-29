a728537a37d6f8222532b58f9c790cf9d68a3e10
Dan Jn <danjan2906@gmail.com>
2025-04-29 17:37:32 +0400

    .env created

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
-from authx import  AuthXConfig
+import os
+from dotenv import load_dotenv
+from authx import AuthXConfig
+
+load_dotenv()
 
 config = AuthXConfig()
-config.JWT_SECRET_KEY = "SECRET_KEY"
+config.JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
 config.JWT_ACCESS_COOKIE_NAME = "my_access_token"
 config.JWT_TOKEN_LOCATION = ["headers"]
