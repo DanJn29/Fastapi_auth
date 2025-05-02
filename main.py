3a7801f9b624ee9668171b08e24cd7e3b34f1548
Dan Jn <danjan2906@gmail.com>
2025-05-02 14:59:50 +0400

    docker file created

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import uvicorn
 from routes import auth_routes, protected_routes
 
 app = FastAPI()
@@ -6,3 +7,6 @@ app = FastAPI()
 
 app.include_router(auth_routes.router, prefix="/auth", tags=["Authentication"])
 app.include_router(protected_routes.router, prefix="/protected", tags=["Protected"])
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
